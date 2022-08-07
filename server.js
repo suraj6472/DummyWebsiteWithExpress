219d103d5fc92e345143eda79c97bdf3dbd68fa3
Suraj Kumar <suraj6472@gmail.com>
2022-08-07 11:22:02 +0530

    default error page added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const router = require('./routes');
 const cookieSession = require('cookie-session');
-
+const createError = require('http-errors');
 const FeedbackService = require('./services/FeedbackService');
 const SpeakerService = require('./services/SpeakerService');
 
@@ -38,6 +38,20 @@ app.use(async (req, res, next) => {
 });
 
 app.use('/', router({ feedbackService, speakerService }));
+
+app.use((req, res, next) => {
+  return next(createError(400, 'Not Found'));
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  console.log(res.locals.message);
+  const status = err.status || 500;
+  res.locals.status = status;
+  res.status(status);
+  res.render('error', { status, message: res.locals.message });
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
